188e15da68f5c09ab6f6a90f296daa8991fb8a07
qucchia <yijods@gmail.com>
2022-01-10 20:24:18 +0100

    Start work on themes

--- src/qonfig/qonfig.app.js
+++ src/qonfig/qonfig.app.js
@@ -3,6 +3,23 @@ let config = require("Storage").readJSON("config.json");
 let qmenu = require("qmenu").init("qonfig.menu");
 qmenu.setPath("home");
 
+// Converts settings to official Settings app format and writes them
+function convertToBangleSettings() {
+  let setting = {};
+  setting.theme = {
+    fg: config.system.theme.foreground,
+    bg: config.system.theme.background,
+    fg2: config.system.theme.foreground2,
+    bg2: config.system.theme.background2,
+    fgH: config.system.theme.fgHighlight,
+    bgH: config.system.theme.bgHighlight,
+    dark: config.system.theme.dark,
+  };
+
+  print(setting);
+  require("Storage").write("setting.json", setting);
+}
+
 qmenu.getDynamicMenu = (path) => {
   if (path === "apps") {
     let items = require("Storage").list(/.app.menu$/).map((file) => {
@@ -40,6 +57,20 @@ qmenu.set = (prop, value) => {
 
   print(config);
 
+  if (prop === "system.themePreset") {
+    config.system.theme = require("Storage").readJSON("themes.json")[value];
+    g.theme = th;
+    settings.theme = th;
+    updateSettings();
+    delete g.reset;
+    g._reset = g.reset;
+    g.reset = function(n) { return g._reset().setColor(th.fg).setBgColor(th.bg); };
+    g.clear = function(n) { if (n) g.reset(); return g.clearRect(0,0,g.getWidth(),g.getHeight()); };
+    g.clear(1);
+    Bangle.drawWidgets();
+  }
+
   require("Storage").write("config.json", config);
+  convertToBangleSettings();
 };
 
